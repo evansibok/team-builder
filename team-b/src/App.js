957db286331b0182920ef546daf8abb2f1849b8e
Evans Ibok <evansibok@gmail.com>
2019-11-13 13:26:40 +0100

    added uuid to use as the id for team list

--- team-b/src/App.js
+++ team-b/src/App.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import uuid from 'uuid';
 import './App.css';
 
 const initialMembers = [
   {
+    id: uuid(),
     fullName: "Chioma",
     email: "chi@gmail.com",
     role: "software engineer"
   },
   {
+    id: uuid(),
     fullName: "Evans",
     email: "evans@gmail.com",
     role: "ux engineer"
@@ -27,7 +30,7 @@ function App() {
     <div className="App">
       {
         members.map(m => {
-          return <div>
+          return <div key={m.id}>
             <h3>{m.fullName}</h3>
             <h4>{m.email}</h4>
             <h4>{m.role}</h4>
