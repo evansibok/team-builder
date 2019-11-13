ec9e429c10606534446bf1985100f31053c13d75
Evans Ibok <evansibok@gmail.com>
2019-11-13 13:15:22 +0100

    set initial state for the team list

--- team-b/src/App.js
+++ team-b/src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const initialMembers = [
+  {
+    fullName: "Chioma",
+    email: "chi@gmail.com",
+    role: "software engineer"
+  },
+  {
+    fullName: "Evans",
+    email: "evans@gmail.com",
+    role: "ux engineer"
+  }
+]
+
+
 function App() {
+
+  const [ members, setMembers ] = useState(initialMembers);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
